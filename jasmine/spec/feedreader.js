186d73ec6bb413ca4d382d0e881fad0b6e9e2f5b
Ashwin Ramachandran <ashwinram1@gmail.com>
2017-03-11 17:34:18 -0800

    all tests complete

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,3 +1,4 @@
+// student name: Ashwin Ramachandran
 /* feedreader.js
  *
  * This is the spec file that Jasmine will read and contains
@@ -32,30 +33,59 @@ $(function() {
          * and that the URL is not empty.
          */
 
+         it('URL for each feed is defined and not empty', function() {
+            for(var index = 0; index < allFeeds.length; index++) {
+                expect((allFeeds[index]).url).toBeDefined();
+                expect((allFeeds[index]).url).not.toBe('');
+            }
+         });
+
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+         it('name for each feed is defined and not empty', function() {
+            for(var index = 0; index < allFeeds.length; index++) {
+                expect((allFeeds[index]).name).toBeDefined();
+                expect((allFeeds[index]).name).not.toBe('');
+            }
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
 
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
-         /* TODO: Write a test that ensures the menu changes
+        /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('changes visibility when the menu icon is clicked', function() {
+            //this line will artificially create a click event to occur so we can test the effects
+            $('.menu-icon-link').trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+
+            $('.menu-icon-link').trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+    });
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
@@ -63,10 +93,45 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+         beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+         });
 
+         it('at least 1 present after loadFeed function is done', function(done) {
+            expect($('.feed .entry').length).toBeGreaterThan(0);
+            done();
+         });
+
+    });
+
+
+    /* TODO: Write a new test suite named "New Feed Selection" */
+
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+         var currentContent, newContent;
+
+         beforeEach(function(done) {
+            // just save the content as is before switiching the default 0 field
+            currentContent = $('.feed').text();
+            loadFeed(1, function() {
+                done();
+            });
+         });
+
+         it('content changes', function(done) {
+            // get the content of the new feed
+            newContent = $('.feed').text();
+            // the old and new content should not be the same
+            expect(currentContent).not.toEqual(newContent);
+            done();
+         });
+    });
+
 }());
